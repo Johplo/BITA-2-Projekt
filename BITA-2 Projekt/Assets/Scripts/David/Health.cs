58deeadce51a9d40fe0a83015f6b79a98702ae6f
DehwitTT <djlueke0901@gmail.com>
2023-08-31 11:14:39 +0200

    David.Health/HealthBar

---
+++ BITA-2 Projekt/Assets/Scripts/David/Health.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Health : MonoBehaviour
+{
+    public int maxHealth = 5;
+    public int currentHealth;
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentHealth = maxHealth;
+    }
+
+    public void TakeDamage(int amount)
+    {
+        currentHealth -= amount;
+
+        if (currentHealth <= 0) 
+        {
+            //Tod
+        }
+    }
+
+    public void Heal(int amount)
+    {
+        currentHealth += amount;
+
+        if (currentHealth > maxHealth)
+        {
+            currentHealth = maxHealth;
+        }
+    }
+}

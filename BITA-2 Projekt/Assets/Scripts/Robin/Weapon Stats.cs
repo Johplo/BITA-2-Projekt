5f728e68afbd8993f1f1a49de8424f9adbc57926
Rapse06 <schwertrobin969@gmail.com>
2023-09-11 12:29:12 +0200

    Robin XYXY
    Weapon Stats Änderung und Überarbeitung der Szenen

--- BITA-2 Projekt/Assets/Scripts/Robin/Weapon Stats.cs
+++ BITA-2 Projekt/Assets/Scripts/Robin/Weapon Stats.cs
@@ -4,17 +4,44 @@ using UnityEngine;
 
 public class WeaponStats : MonoBehaviour
 {
-
     #region Robin
+    public GameObject centerpos;
 
     public float damage;
     public float speed;
+    public float range;
+    public float cooldown;
 
-    public void AddWeapon(float _damage, float _speed)
+    private void Start()
+    {
+        if (centerpos == null)
+        {
+            centerpos = transform.Find("Center").gameObject;
+        }
+    }
+
+    public void AddWeapon(float _damage, float _speed, float _range, float _cooldown)
     {
         this.damage = _damage;
         this.speed = _speed;
+        this.range = _range;
+        this.cooldown = _cooldown;
     }
 
+    private void Update()
+    {
+        Vector3 Mousepos = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, 0));
+        Vector3 direction = (Mousepos - transform.position) * 200;
+        direction.Normalize();
+        if (Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            RaycastHit2D hit = Physics2D.Raycast(transform.position, direction, range);
+            if(hit.collider != null)
+            {
+                Debug.LogWarning("doDamage");
+            }
+            Debug.DrawRay(transform.position, direction * range, Color.red);
+        }
+    }
     #endregion
 }

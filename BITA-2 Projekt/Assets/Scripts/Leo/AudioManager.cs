e4af41dfccf8bfbe2e0cf1ceab50646036517932
Hernri-Leo <henrileo.bauer@gmx.de>
2023-11-27 12:50:13 +0100

    mp4 sound musik
    sounds bearbeitet
    musik verucht zu spielen aber ohne erfolg
    teich texture gif mp4 auch ohne erfolg

--- BITA-2 Projekt/Assets/Scripts/Leo/AudioManager.cs
+++ BITA-2 Projekt/Assets/Scripts/Leo/AudioManager.cs
@@ -6,8 +6,25 @@ public class AudioManager : MonoBehaviour {
     
     public Sound[] sounds;
 
-    
+    public static AudioManager instance;
+
+
+
+
+
     void Awake() {
+
+        if (instance == null)
+            instance = this;
+        else
+        {
+            Destroy(gameObject);
+            return;
+        }
+        
+
+        DontDestroyOnLoad(gameObject);
+
         foreach(Sound s in sounds)
         {
             s.source = gameObject.AddComponent<AudioSource>();
@@ -22,11 +39,22 @@ public class AudioManager : MonoBehaviour {
     void Start()
     {
         Play("Theme");
+        Play("Theme1"); 
+
     }
+
+    
+
+
 
     public void Play (string name)
     {
         Sound s = Array.Find(sounds, sound => sound.name == name);
+        if (s == null)
+        {
+            Debug.LogWarning("Sound: " + name + " konte nicht gefunden werden!");
+            return;
+        }
         s.source.Play();
 
 
